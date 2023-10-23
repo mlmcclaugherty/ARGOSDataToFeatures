22e554fb852094f8715f26f235fa46705c70e7d3
mlmcclaugherty <megan.mcclaugherty@duke.edu>
2023-10-23 10:29:42 -0400

    Import packages and set variables

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -9,3 +9,10 @@
 ## Created: Fall 2023
 ## Author: megan.mcclaugherty@duke.edu for ENV859
 ##---------------------------------------------------------------------
+
+# Import modules
+import sys, os, arcpy
+
+# Set input variable (hard-wired)
+inputFile = 'V:/ARGOSTracking/Data/ARGOSdata/1997dg.txt'
+outputFC = V:/ARGOSTracking/Scratch/ARGOSTrack.shp
